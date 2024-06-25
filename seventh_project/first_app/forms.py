d756d69c8af8b1083b5c4e275773f2a653900612
Md Mehedi Hasan <mdmehedihasan1515@gmail.com>
2024-06-25 20:51:18 +0600

    seventh project added

---
+++ seventh_project/first_app/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from first_app.models import StudentModel
+
+
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = StudentModel
+        
+        fields = '__all__'
+        # exclude = ['roll']
+        
+        labels = {
+            'name': 'Student Name',
+            'roll': 'Student Roll'
+        }
+        
+        widgets = {
+            
+            'name' : forms.TextInput(),
+            'roll' : forms.PasswordInput()
+        }
+        
+        help_texts = {
+            'name': "write your full name"
+        }
+        
+        error_messages = {
+            'name': {'required': 'Your name is required'}
+        }
